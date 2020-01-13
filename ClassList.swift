27abc9de3f75ba4a8d14be3244bfaf98f87aca4e
Bogzzz <44644148+Bogzzz@users.noreply.github.com>
2020-01-13 15:14:03 -0500

    Add files via upload

---
+++ ClassList.swift
@@ -0,0 +1,29 @@
+//
+//  ClassList.swift
+//  MyApp
+//
+//  Created by Aleksandr Stepanenko on 2020-01-13.
+//  Copyright © 2020 dev. All rights reserved.
+//
+
+import UIKit
+
+class ClassList: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
